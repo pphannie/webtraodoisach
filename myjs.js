30ed9bc3c66caa977d92f2b2d9bfbdd565f433a4
KhangNH-123 <khangb2303820@student.ctu.edu.vn>
2025-07-12 01:03:19 +0700

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -4,18 +4,8 @@ const prevBtn = document.getElementById("prevBtn");
 
 let currentIndex = 0;
 
-function getBookWidth() {
-  const book = bookList.querySelector(".book");
-  const style = window.getComputedStyle(book);
-  const marginLeft = parseInt(style.marginLeft) || 0;
-  const marginRight = parseInt(style.marginRight) || 0;
-  return book.offsetWidth + marginLeft + marginRight;
-}
-
-function getMaxIndex() {
-  const books = bookList.children.length;
-  const itemsPerView = getItemsPerView();
-  return books - itemsPerView;
+function getBooks() {
+  return Array.from(bookList.querySelectorAll(".book")); // chỉ lấy .book, bỏ #dot
 }
 
 function getItemsPerView() {
@@ -23,6 +13,20 @@ function getItemsPerView() {
   if (width >= 1024) return 5;
   if (width >= 768) return 3;
   return 1;
+}
+
+function getMaxIndex() {
+  const books = getBooks().length;
+  const itemsPerView = getItemsPerView();
+  return books - itemsPerView;
+}
+
+function getBookWidth() {
+  const book = bookList.querySelector(".book");
+  const style = window.getComputedStyle(book);
+  const marginLeft = parseInt(style.marginLeft) || 0;
+  const marginRight = parseInt(style.marginRight) || 0;
+  return book.offsetWidth + marginLeft + marginRight;
 }
 
 function updateCarousel() {
